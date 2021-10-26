241b63f902b84ef559bb7a4b9c0ad585fabe706a
奚文浩 <xiwenhao@qianxin.com>
2021-10-26 17:28:24 +0800

    fix: 点歌cqcode错误

--- gocqhttp/server.py
+++ gocqhttp/server.py
@@ -62,13 +62,8 @@ def on_message(ws, message):
         send_group_msg(msg["group_id"], getoutput("git rev-parse HEAD"))
 
     if op == "点歌":
-        send_group_msg(msg["group_id"], json.dumps({
-            "type": "music",
-            "data": {
-                "type": "qq",
-                "id": "000MDaNK0krdFb"
-            }
-        }))
+        send_group_msg(msg["group_id"], "[CQ:music,type=qq,id=000MDaNK0krdFb]")
+
 
 
 def on_error(ws, error):
