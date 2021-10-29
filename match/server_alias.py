cc18673a8b0cc8451c5d81a24e810eca27848494
奚文浩 <xiwenhao@qianxin.com>
2021-10-29 18:33:21 +0800

    fix: 支持服务器俗名

--- match/server_alias.py
+++ match/server_alias.py
@@ -5,18 +5,12 @@ from exception import ServerNotFound
 
 alias_server_mapping = {
     "全部": "全部",
-    "天鹅坪": "纵月",
-    "唯我独尊": "唯满侠",
+    "纵月": "天鹅坪",
+    "唯满侠": "唯我独尊"
 }
-server_alias_mapping = {v: k for v, k in alias_server_mapping.items()}
+server_alias_mapping = {v: k for k, v in alias_server_mapping.items()}
 
-def alias2server(alias):
-    if alias in alias_server_mapping:
-        return alias_server_mapping[alias]
-    else:
-        return None
-
-def server2alias(server_alias):
+def alias2server(server_alias):
     """
     :param server_alias: 服务器俗名
     :return: 主服务器名
@@ -25,3 +19,11 @@ def server2alias(server_alias):
         return server_alias_mapping[server_alias]
     if server_alias in alias_server_mapping:
         return server_alias
+
+
+def server2alias(server):
+    return server_alias_mapping[server]
+
+
+if __name__ == '__main__':
+    print(server2alias("天鹅坪"))
